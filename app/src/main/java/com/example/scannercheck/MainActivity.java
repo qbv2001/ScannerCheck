556f9422dd45059e061def507a69934c1fb2a773
NguyenVan15 <92082858+NguyenVan15@users.noreply.github.com>
2021-10-29 08:29:11 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/scannercheck/MainActivity.java
+++ app/src/main/java/com/example/scannercheck/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //Button sign_in;
+
+    //AccountAuthParams authParams;
+    //AccountAuthService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
