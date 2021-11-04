61dbfadaa5e8c8a623b22648b4c9c99da21774f9
qbv2001 <huutinh999999@gmail.com>
2021-11-05 03:05:35 +0700

    add acckit

--- app/src/main/java/com/example/scannercheck/MainActivity.java
+++ app/src/main/java/com/example/scannercheck/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.scannercheck;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AccountAuthParams authParams = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM).setIdToken().createParams();
+        AccountAuthService service = AccountAuthManager.getService(MainActivity.this, authParams);
+        startActivityForResult(service.getSignInIntent(), 8888);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 8888) {
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            if (authAccountTask.isSuccessful()) {
+                AuthAccount authAccount = authAccountTask.getResult();
+                Log.i("truong", "idToken:" + authAccount.getIdToken());
+            } else {
+                // The sign-in failed. No processing is required. Logs are recorded for fault locating.
+                Log.e("truong", "sign in failed : " +((ApiException) authAccountTask.getException()).getStatusCode());
+            }
+        }
     }
 }
