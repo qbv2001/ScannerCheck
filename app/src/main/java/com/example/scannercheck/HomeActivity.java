9253ac943b1b1e642516a46c100685263e6df70e
qbv2001 <huutinh999999@gmail.com>
2021-11-07 14:57:17 +0700

    scan kit Tinh

--- app/src/main/java/com/example/scannercheck/HomeActivity.java
+++ app/src/main/java/com/example/scannercheck/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.scannercheck;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,8 +17,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.huawei.hms.hmsscankit.ScanUtil;
+import com.huawei.hms.ml.scan.HmsScan;
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final int DEFAULT_VIEW = 0x22;
+
+    private static final int REQUEST_CODE_SCAN = 0X01;
+
     private CardView scanner,dsmh,dsncc,thongke,profile,logout,thongtin,hotro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,9 +91,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 case R.id.nav_ttud:
                     i1 = new Intent(this, ThongtinungdungActivity.class);startActivity(i1);
                     break;
-                case R.id.nav_scanner:
-                    i1 = new Intent(this, ScannerActivity.class);startActivity(i1);
-                    break;
                 case R.id.nav_ttcn:
                     i1 = new Intent(this, ThongtintaikhoanActivity.class);startActivity(i1);
                     break;
@@ -126,4 +135,46 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             default: break;
         }
     }
+
+    public void newViewBtnClick(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.requestPermissions(
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    DEFAULT_VIEW);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (permissions == null || grantResults == null || grantResults.length < 2 || grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        if (requestCode == DEFAULT_VIEW) {
+            //start ScankitActivity for scanning barcode
+            ScanUtil.startScan(HomeActivity.this, REQUEST_CODE_SCAN, new HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).create());
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //receive result after your activity finished scanning
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK || data == null) {
+            return;
+        }
+        // Obtain the return value of HmsScan from the value returned by the onActivityResult method by using ScanUtil.RESULT as the key value.
+        if (requestCode == REQUEST_CODE_SCAN) {
+            Object obj = data.getParcelableExtra(ScanUtil.RESULT);
+            if (obj instanceof HmsScan) {
+                if (!TextUtils.isEmpty(((HmsScan) obj).getOriginalValue())) {
+                    Toast.makeText(this, ((HmsScan) obj).getOriginalValue(), Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
 }
