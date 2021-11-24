fe158f0e8cb4b3fd6a3fe4077e70194bde5ffab9
qbv2001 <huutinh999999@gmail.com>
2021-11-24 16:25:48 +0700

    xu ly dang nhap

--- app/src/main/java/com/example/scannercheck/LoginActivity.java
+++ app/src/main/java/com/example/scannercheck/LoginActivity.java
@@ -1,39 +1,84 @@
 package com.example.scannercheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnlogin;
+    TextView btn;
+    EditText edtEmail,edtPassword;
 
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        initUI();
         signupclick();
         signinclick();
 
     }
 
-    private void signinclick() {
-        Button btnlogin = findViewById(R.id.btnlogin);
+    private void initUI(){
+        progressDialog = new ProgressDialog(this);
+        edtEmail = findViewById(R.id.inputEmail);
+        edtPassword = findViewById(R.id.inputPassword);
+        btnlogin = findViewById(R.id.btnlogin);
+        btn = findViewById(R.id.textViewSignUp);
 
+    }
+
+    private void signinclick() {
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                checkdangnhap();
             }
         });
     }
 
-    private void signupclick() {
-        TextView btn = findViewById(R.id.textViewSignUp);
+    private void checkdangnhap() {
 
+        String strEmail = edtEmail.getText().toString().trim();
+        String strPassword = edtPassword.getText().toString().trim();
+
+        progressDialog.show();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(strEmail, strPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thành công.",
+                                    Toast.LENGTH_SHORT).show();
+                            finishAffinity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Đăng nhập thất bại.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signupclick() {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
