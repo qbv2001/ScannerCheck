2ecd56f45daa1fb8e17e14551754ab2ef02b8340
qbv2001 <huutinh999999@gmail.com>
2021-11-29 23:59:48 +0700

    hoan thanh

--- app/src/main/java/com/example/scannercheck/ThongkeActivity.java
+++ app/src/main/java/com/example/scannercheck/ThongkeActivity.java
@@ -2,9 +2,11 @@ package com.example.scannercheck;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -29,22 +31,35 @@ public class ThongkeActivity extends AppCompatActivity {
 
     private FirebaseUser user;
     private DatabaseReference dataUser;
-
+    private DatabaseReference data;
     NavigationView mNavigationView;
     CircleImageView imgprofilepic;
     TextView tvname;
     TextView tvuseremail;
 
+    TextView thongkeMH;
+    TextView thongkeNCC;
+
+    private int tongMH,tongNCC;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongke);
+        data = FirebaseDatabase.getInstance().getReference();
         dataUser = FirebaseDatabase.getInstance().getReference();
         user = FirebaseAuth.getInstance().getCurrentUser();
         initUi();
+        tongMH = 0;
+        tongNCC = 0;
         showUserInfo();
         readDatabaseUser();
+        readDatabaseNCC();
+        readDatabaseMH();
 
+        thongkeMH.setText("Tổng số mặt hàng đã hết: "+tongMH);
+        thongkeMH.setText("Tổng số nhà cung cấp: "+tongNCC);
         // sidebar
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
@@ -106,7 +121,56 @@ public class ThongkeActivity extends AppCompatActivity {
         });
     }
 
+    private void readDatabaseMH() {
+        // Read from the database
+        data.child("MatHang").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Mathang value = new Mathang();
+                for (DataSnapshot unit : dataSnapshot.getChildren()){
+                    value = unit.getValue(Mathang.class);
+                    if(value.getSoluong()==0){
+                        tongMH++;
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(ThongkeActivity.this, "Đọc thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void readDatabaseNCC() {
+        // Read from the database
+        data.child("NhaCungCap").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot unit : dataSnapshot.getChildren()){
+                    tongNCC++;
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(ThongkeActivity.this, "Đọc thất bại!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void initUi(){
+        thongkeMH = findViewById(R.id.thongkemathang);
+        thongkeNCC = findViewById(R.id.thongkenhacungcap);
+
         mNavigationView = findViewById(R.id.navigation_view);
         imgprofilepic = mNavigationView.getHeaderView(0).findViewById(R.id.profilepic);
         tvname = mNavigationView.getHeaderView(0).findViewById(R.id.name);
